3be45beabcc4fed379de931d42c3ec02632616eb
Munawar Ahmad <anwarahmad391@gmail.com>
2022-08-11 13:42:29 +0700

    Commit pertama

---
+++ lib/0-latihan.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Column & Row"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Text 1"),
+            Text("Text 2"),
+            Text("Text 3"),
+            Row(
+              children: [Text("Text-4"), Text("Text-5"), Text("Text-6")],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/2-text_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Widget Text"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 200,
+                height: 100,
+                child: Text(
+                    "Hello World, Pada kesempatan ini saya sedang melatih kemampuan flutter saya",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16)))),
+      ),
+    );
+  }
+}
